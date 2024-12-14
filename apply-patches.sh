0204912f712c6c7475c3f20170bd569c2b485d4a
mytja <mitja@severkar.eu>
2024-12-14 20:03:03 +0100

    Add a better patch application script
    Co-authored-by: Alberto Ponces <ponces26@gmail.com>
    Change-Id: I1abafd3241a1226046964a3b1e94c8981ce73bb5

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,64 +1,19 @@
 #!/bin/bash
 
-# TrebleDroid patches are TrebleDroid's patches_for_developers.zip
-# TrebleDroid personal patches are TrebleDroid's patches, except that they apply on top of other patches on EvolutionX's source
-# Personal patches are my own patches, mostly to ensure EvolutionX compatibility
-
 set -e
 
-source="$(readlink -f -- $1)"
-patches_directory="$source/patches"
+patches="$(readlink -f -- $1)"
+tree="$2"
 
-apply_directory=$2
-if [[ -z "$apply_directory" ]]; then
-	apply_directory=$(cd $patches_directory; echo *)
-elif [[ "$apply_directory" == trebledroid ]]; then
-	apply_directory="0001-TrebleDroid"
-elif [[ "$apply_directory" == personal ]]; then
-	apply_directory="0002-personal"
-elif [[ ! -d "$apply_directory" ]]; then
-	echo "The directory $apply_directory doesn't exist!"
-	exit 1
-fi
-
-for patch_source in $apply_directory; do
-	if [[ "$patch_source" == apply-patches.sh ]]; then
-		continue
-	fi
-	if [[ "$patch_source" == README.md ]]; then
-		continue
-	fi
-
-	printf "\n ### APPLYING $patch_source PATCHES ###\n";
-	sleep 1.0;
-	for path in $(cd $patches_directory/$patch_source; echo *); do
-		tree="$(tr _ / <<<$path | sed -e 's;platform/;;g')"
-		printf "\n| $path ###\n";
-		[ "$tree" == build ] && tree=build/make
-		[ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
-		[ "$tree" == treble/app ] && tree=treble_app
-		pushd $source/$tree
-
-		for patch in $patches_directory/$patch_source/$path/*.patch; do
-			# Check if patch is already applied
-			if patch -f -p1 --dry-run -R < $patch > /dev/null; then
-				printf "### RECHECK IF APPLIED!!!!!!!!!!!!: $patch \n";
-				continue
-			fi
-
-			if git apply --check $patch; then
-				git am $patch
-			elif patch -f -p1 --dry-run < $patch > /dev/null; then
-				#This will fail
-				git am $patch || true
-				patch -f -p1 < $patch
-				git add -u
-				git am --continue
-			else
-				printf "### FAILED APPLYING: $patch \n"
-			fi
-		done
-
-		popd
-	done
+for project in $(cd $patches/patches/$tree; echo *); do
+    p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == treble/app ] && p=treble_app
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    pushd $p &>/dev/null
+    for patch in $patches/patches/$tree/$project/*.patch; do
+        git am --reject $patch || read -p "--- Patch $patch needs manual application. Waiting for the patch to be applied."
+    done
+    popd &>/dev/null
 done
+
