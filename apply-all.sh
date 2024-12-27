d2d39db57a366316f3be1e2409da0ec9dd4cb83a
mytja <mitja@severkar.eu>
2024-12-27 20:29:40 +0100

    Add additional patches
    Change-Id: Ida6fffb16690daef47a8a138abcbadf2bccfd34b

--- apply-all.sh
+++ apply-all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 patches="$(readlink -f -- $1)"
 
+./patches/apply-patches.sh $patches lineage-prerequisite
 ./patches/apply-patches.sh $patches trebledroid
-./patches/apply-patches.sh $patches trebledroid-corrections # TODO: Merge this with respective patches in TrebleDroid. IT SHOULD ONLY BE TEMPORARY!!!!!
-./patches/apply-patches.sh $patches trebledroid-backports
+./patches/apply-patches.sh $patches trebledroid-backport
 ./patches/apply-patches.sh $patches personal
+
